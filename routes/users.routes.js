9f44eaa8ba6591ba16f8c14f0b3f212bcddf44d8
Cesar <cessardevs@gmail.com>
2026-04-16 14:20:55 +0100

    feat: updated route to only obtain details for ironhackers and to be only available for companies

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -18,15 +18,15 @@ router.get("/ironhackers", isAuthenticated, isCompany, (req, res, next) => {
 });
 
 // GET /api/users/:userId
-// Obtiene los detalles de un usuario específico por su ID
-router.get("/:userId", isAuthenticated, (req, res, next) => {
+// Obtiene los detalles de un usuario específico si es IRONHACKER
+router.get("/:userId", isAuthenticated, isCompany, (req, res, next) => {
   const { userId } = req.params;
 
   User.findById(userId)
     .select("-password")
     .then((user) => {
-      if (!user) {
-        return res.status(404).json({ message: "User not found" });
+      if (!user || user.role !== "IRONHACKER") {
+        return res.status(404).json({ message: "Ironhacker not found" });
       }
       res.status(200).json(user);
     })
